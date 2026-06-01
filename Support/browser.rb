6cbc5acc9a77327bded33a2840445b685473de76
Shane Becker <veganstraightedge@gmail.com>
2026-05-31 23:42:31 -0700

    shebangs: ruby18/ruby20 -> ruby; drop 1.8-only flags (-K*, -rjcode)

--- Support/browser.rb
+++ Support/browser.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby18
+#!/usr/bin/env ruby
 PROJECT_ROOT = ARGV[0] # Where to browse for images
 RESOURCES_DIR = "#{ENV['TM_BUNDLE_SUPPORT']}/Resources" # Where to find ImageBrowser's images
 require "#{ENV['TM_BUNDLE_SUPPORT']}/image_size.rb"
